ce001464c88c3f14ec2a5ca9dcdbe2cc8eb9621c
adiaux <aditya8anil@gmail.com>
2021-04-27 15:32:15 +0530

    New file

--- spider.py
+++ spider.py
@@ -26,21 +26,22 @@ def collectLabels(driver):
 def collectValues(driver):
     return driver.find_elements_by_xpath("//td[contains(@class,'spec_des')]")
 
-info=openFile("outputs/step1.json")
+info=openFile("outputs/step1_part_a.json")
 fin=[]
 done=0
-for i in info:
-    mini_driver=startDriver(info[i])
-    dic={}
-    dic["Name"]=mini_driver.find_element_by_xpath("//h1[contains(@class,'h1_pro_head')]").text
-    for label,values in zip(collectLabels(mini_driver),collectValues(mini_driver)):
-        dic[label.text]=values.text
-    fin.append(dic)
-    done+=1
-    print(done)
-    mini_driver.close()
-    #mini_driver.close()
-
-with open("outputs/step2.json","w") as op:
-    json.dump(fin, op)
+try:
+    for i in info:
+        mini_driver=startDriver(info[i])
+        dic={}
+        dic["Name"]=mini_driver.find_element_by_xpath("//h1[contains(@class,'h1_pro_head')]").text
+        for label,values in zip(collectLabels(mini_driver),collectValues(mini_driver)):
+            dic[label.text]=values.text
+        fin.append(dic)
+        done+=1
+        print(done)
+        mini_driver.close()
+        #mini_driver.close()
+finally:
+    with open("outputs/step2.json","w") as op:
+        json.dump(fin, op)
 
